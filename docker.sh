736ca017d71139bdcc95bd9b562aae3383749bf1
F5 Nodes <94526481+f5nodes@users.noreply.github.com>
2022-02-09 16:12:30 +0200

    first commit

---
+++ docker.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Default variables
+function="install"
+
+# Options
+. <(wget -qO- https://raw.githubusercontent.com/f5nodes/logo/main/colors.sh) --
+option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
+while test $# -gt 0; do
+	case "$1" in
+	-h|--help)
+		. <(wget -qO- https://raw.githubusercontent.com/f5nodes/logo/main/logo.sh)
+		echo
+		echo -e "${C_LGn}Functionality${RES}: the script installs or uninstalls Docker"
+		echo
+		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
+		echo
+		echo -e "${C_LGn}Options${RES}:"
+		echo -e "  -h, --help       show the help page"
+		echo -e "  -u, --uninstall  uninstall Docker (${C_R}completely delete all images and containers${RES})"
+		echo
+		return 0 2>/dev/null; exit 0
+		;;
+	-u|--uninstall)
+		function="uninstall"
+		shift
+		;;
+	*|--)
+		break
+		;;
+	esac
+done
+
+# Functions
+install() {
+	echo -e "${C_LGn}Docker installation...${RES}"
+	cd
+	if ! docker --version; then
+		sudo apt update
+		sudo apt upgrade -y
+		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu `lsb_release -cs` stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		sudo apt update
+		sudo apt install docker-ce docker-ce-cli containerd.io -y
+		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
+		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
+	fi
+	if ! dpkg -s dive | grep -q "ok installed"; then
+		echo -e "${C_LGn}Dive installing...${RES}"
+		wget https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.deb
+		sudo apt install ./dive_0.9.2_linux_amd64.deb
+		rm -rf dive_0.9.2_linux_amd64.deb
+	fi
+}
+uninstall() {
+	echo -e "${C_LGn}Docker uninstalling...${RES}"
+	sudo dpkg -r dive
+	sudo systemctl stop docker.service
+	sudo systemctl stop docker.socket
+	sudo rm -rf `systemctl cat docker.service | grep -oPm1 "(?<=^#)([^%]+)"` `systemctl cat docker.socket | grep -oPm1 "(?<=^#)([^%]+)"`
+	sudo apt purge docker-engine docker docker.io docker-ce docker-ce-cli -y
+	sudo apt autoremove --purge docker-engine docker docker.io docker-ce -y
+	sudo apt autoclean
+	sudo rm -rf /var/lib/docker /etc/appasudo rmor.d/docker
+	sudo groupdel docker
+	sudo rm -rf /etc/docker /usr/bin/docker /usr/libexec/docker /usr/libexec/docker/cli-plugins/docker-buildx /usr/libexec/docker/cli-plugins/docker-scan /usr/libexec/docker/cli-plugins/docker-app /usr/share/keyrings/docker-archive-keyring.gpg
+}
+
+# Actions
+$function
+echo -e "${C_LGn}Done!${RES}"
