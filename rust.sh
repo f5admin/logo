bf954fc6a95a81b84c1b35ca0e7c85564ba96e97
F5 Nodes <94526481+f5nodes@users.noreply.github.com>
2022-02-09 16:20:52 +0200

    first commit

---
+++ rust.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Default variables
+function="install"
+nightly="false"
+
+# Options
+. <(wget -qO- https://raw.githubusercontent.com/f5nodes/logo/main/colors.sh) --
+option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
+while test $# -gt 0; do
+	case "$1" in
+	-h|--help)
+		. <(wget -qO- https://raw.githubusercontent.com/f5nodes/logo/main/logo.sh)
+		echo
+		echo -e "${C_LGn}Functionality${RES}: the script installs or uninstalls Rust"
+		echo
+		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
+		echo
+		echo -e "${C_LGn}Options${RES}:"
+		echo -e "  -h, --help       show the help page"
+		echo -e "  -n, --nightly    install nightly version of Rust"
+		echo -e "  -u, --uninstall  uninstall Rust"
+		echo
+		echo -e "https://github.com/f5nodes/"
+		echo -e "https://t.me/+ll8AINfEBWNkYjky — node community"
+		echo
+		return 0 2>/dev/null; exit 0
+		;;
+	-n|--nightly)
+		nightly="true"
+		shift
+		;;
+	-u|--uninstall)
+		function="uninstall"
+		shift
+		;;
+	*|--)
+		break
+		;;
+	esac
+done
+
+# Functions
+install() {
+	echo -e "${C_LGn}Rust installation...${RES}"
+	sudo apt update
+	sudo apt upgrade -y
+	sudo apt install curl build-essential pkg-config libssl-dev libudev-dev clang make -y
+	curl --proto '=https' -sSf https://sh.rustup.rs | sh -s -- -y
+	echo -e "${C_R}^ X Don't do that X ^${RES}\n"
+	. $HOME/.cargo/env
+	if [ "$nightly" = "true" ]; then
+		rustup toolchain install nightly
+		rustup default nightly
+	fi
+}
+uninstall() {
+	echo -e "${C_LGn}Rust uninstalling...${RES}"
+	rustup self uninstall -y
+}
+
+# Actions
+$function
+echo -e "${C_LGn}Done!${RES}"
